d9a6e168e3e131f8df8a4e558d32a81187de141c
Thomas Herzog <batthomas@gmx.ch>
2026-03-18 00:49:53 +0100

    Not yet

--- src/tt_um_plasmanim.sv
+++ src/tt_um_plasmanim.sv
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_plasmanim (
     input  logic [7:0] ui_in,    // Dedicated inputs
     output logic [7:0] uo_out,   // Dedicated outputs
     input  logic [7:0] uio_in,   // IOs: Input path
